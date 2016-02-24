dec8ca3437435acccc0ee6b2ef30b1e1540b7582
Shamsher <ansarisam47@gmail.com>
2016-02-24 22:23:22 +0530

    Add benchmark test

--- root_test.go
+++ root_test.go
@@ -2,8 +2,16 @@ package root
 
 import "testing"
 
+// go test -v
 func TestIs(t *testing.T) {
 	if Is() == true {
 		t.Fatal("Should return true")
+	}
+}
+
+// go test -test.bench=".*"
+func BenchmarkIs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Is()
 	}
 }
